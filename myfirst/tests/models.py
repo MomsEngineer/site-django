406c5adfe82f97c7da2899a82aa4f54a75d16bbb
kirill <Mom's Engineer>
2020-10-13 14:14:42 +0300

    M-2 | Create models
    Created models and added them
    to DB

--- myfirst/tests/models.py
+++ myfirst/tests/models.py
@@ -1,3 +1,24 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Test(models.Model):
+    test_title = models.CharField('название статьи', max_length = 200)
+    test_text  = models.TextField('текст теста')
+    pub_date   = models.DateTimeField('дата публикации')
+
+    def __str__(self):
+        return self.test_title
+
+    def was_published_recently(self):
+        return self.pub_date >= (timezone.now() - datetime.timedelta(days = 7))
+
+class Comment(models.Model):
+    test         = models.ForeignKey(Test, on_delete = models.CASCADE)
+    author_name  = models.CharField('имя автора', max_length = 50)
+    comment_text = models.TextField('текст коментария', max_length = 200)
+
+    def __str__(self):
+        return self.author_name
+
+
